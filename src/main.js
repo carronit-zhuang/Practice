a8085d13f256b34c5d54f6c0fb1667470c09420e
carronit.zhuang <carronit.zhuang@gz.questwork.com>
2019-12-31 18:10:05 +0800

    项目第一天

--- src/main.js
+++ src/main.js
@@ -1,18 +1,34 @@
 // 入口文件
 import Vue from "vue";
 
+// 1.1 导入路由的包
+import VueRouter from 'vue-router';
+// 1.2 安装路由
+Vue.use(VueRouter);
+// 1.3 引入自己的router.js路由模块
+import router from './router.js';
+
+// 2.1 导入vue-resource
+import VueResource from 'vue-resource';
+// 2.2 安装vue-resource
+Vue.use(VueResource);
+
 // 引入app组件
 import app from './App.vue';
 
 // 按需要导入mint-ui中的组件
-import {Header} from 'mint-ui';
+import {Header,Swipe, SwipeItem} from 'mint-ui';
 // 注册组件
 Vue.component(Header.name,Header);
+Vue.component(Swipe.name, Swipe);
+Vue.component(SwipeItem.name, SwipeItem);
 
 // 导入MUI的样式
 import './lib/mui/css/mui.min.css';
+import './lib/mui/css/icons-extra.css'
 
 var vm = new Vue({
   el: '#app',
-  render: c => c(app)
+  render: c => c(app),
+  router //1.4 挂载路由对象到vm实例上
 })
--- src/router.js
+++ src/router.js
@@ -1,9 +1,22 @@
 import VueRouter from "vue-router";
 
+// 导入对应的路由组件
+import Homecontainer from './components/Homecontainer.vue'
+import Membercontainer from './components/Membercontainer.vue'
+import Shopcarcontainer from './components/Shopcarcontainer.vue'
+import Searchcontainer from './components/Searchcontainer.vue'
 
+
+// 创建路由对象
 var router = new VueRouter({
-  routes: [
-
-  ]
+  routes: [   //配置路由的匹配规则
+      {path: '/home', component: Homecontainer },
+      {path: '/member', component: Membercontainer},
+      {path: '/shopcar', component: Shopcarcontainer},
+      {path: '/search', component: Searchcontainer},
+      {path: '/', redirect: '/home'},
+  ],
+  linkActiveClass: 'mui-active',//替换默认的路由高亮的类，原类名叫做 router-link-active
 });
+// 把自己创建好的路由对象暴露出去，使其得以挂载到VM实例上面
 export default router;
